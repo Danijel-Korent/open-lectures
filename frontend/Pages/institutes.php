8d4bc966afdd7bce8b3f3957c29e420b17b9d86c
Marija Milosevic <80062620+marmilosev@users.noreply.github.com>
2022-03-06 00:11:35 +0100

    Frontend without data on website.
    Backend and frontend are not yet joined so there is no data on the website.

---
+++ frontend/Pages/institutes.php
@@ -0,0 +1,24 @@
+<?php require "../Components/header.html"; ?>
+
+<div class="grid-x grid-margin-x">
+  <div class="cell large-12 small-12">
+    <h3>INSTITUTES</h3>
+  </div>
+</div>
+
+<div class="grid-x grid-margin-x">
+  <div class="cell large-4 small-6">
+    <img>
+    <p>Institucija #1</p>
+  </div>
+  <div class="cell large-4 small-6">
+  <img>
+  <p>Institucija #2</p>
+  </div>
+  <div class="cell large-4 small-6">
+  <img>
+  <p>Institucija #3</p>
+  </div>
+</div>
+
+<?php require "../Components/footer.html"; ?>
