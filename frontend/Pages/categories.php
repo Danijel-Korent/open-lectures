d656f078252594abdc09e644b8c7535263789d7a
marmilosev <mmilosevic@ffos.hr>
2022-03-06 16:48:01 +0100

    Frontend with data from backend

--- frontend/Pages/categories.php
+++ frontend/Pages/categories.php
@@ -1,24 +1,31 @@
-<?php require "../Components/header.html"; ?>
+<?php 
+require "../Components/header.html";
+require "../../backend/index.php";
+?>
 
 <div class="grid-x grid-margin-x">
-  <div class="cell large-12 small-12">
+  <div class="cell large-12 medium-12 small-12">
     <h3>CATEGORIES</h3>
   </div>
 </div>
 
+<?php 
+foreach($arrayKategorije as $kategorija){ 
+?>
+
 <div class="grid-x grid-margin-x">
-  <div class="cell large-4 small-6">
-    <img>
-    <p>Kategorija #1</p>
-  </div>
-  <div class="cell large-4 small-6">
-  <img>
-  <p>Kategorija #2</p>
-  </div>
-  <div class="cell large-4 small-6">
-  <img>
-  <p>Kategorija #3</p>
+  <div class="cell large-4 medium-4 small-6">
+  <p><a href="certain_category.php?id=<?php echo $kategorija["idKategorije"] ?>">
+  <img src="../Image/<?php 
+              if($kategorija["slika_kategorije"] == ""){
+                  echo "Nema slike";
+                  }else{
+                      echo $kategorija["slika_kategorije"];
+                 } ?>">
+    <?php echo $kategorija["naziv_kategorije"] ?></a></p>
   </div>
 </div>
+
+<?php } ?>
 
 <?php require "../Components/footer.html"; ?>
