5f654b1d97b50d4a6d3737d78c4597a79978eee8
Benjamin. <dotsehyde@gmail.com>
2024-09-20 10:21:28 +0000

    🌈 style: Populated homepage with content

--- new/database/repo.php
+++ new/database/repo.php
@@ -2,13 +2,16 @@
 require_once 'config.php';
 function selectUstanove(){
 	$res = db()->query("SELECT * FROM ustanove");
-	// Check if the query was successful
 	if (db()->error) {
 	    echo 'DB Error: ' . db()->error;
 		die();
 	} else {
 	//return the result
-	    return $res->fetch_assoc();
+	$arr =[];
+	while($row = $res->fetch_assoc()){
+		$arr[] = $row;
+	}
+	    return $arr;
 	}
 };
 
@@ -27,3 +30,36 @@ function selectKategorije(){
 	    return $arr;
 	}
 };
+
+function selectOpisPred (){
+    $res = db()->query("SELECT pred.naziv_predavanja, u.naziv_ustanove,
+    p.ime, p.prezime,  pred.jezik, pred.broj_predavanja, 
+    pred.ukupno_trajanje, pred.oznaka, pred.oznaka, pred.opis_kolegija, pred.link_1, pred.link_2, pred.image, prip.kategorije, z.ustanova
+    FROM ustanove u 
+    INNER JOIN zaposlenje z ON u.idUstanove = z.ustanova
+    INNER JOIN predavaci p ON p.idPredavac = z.predavac
+    INNER JOIN lekcije l on l.predavac = p.idPredavac
+    INNER JOIN predavanja pred ON pred.idPredavanja = l.predavanja
+    INNER JOIN pripadnost_kategoriji prip ON pred.idPredavanja = prip.predavanje
+    INNER JOIN kategorije k ON k.idKategorije = prip.kategorije;
+    ");
+   if (db()->error) {
+		echo 'DB Error: ' . db()->error;
+		die();
+	} else {
+		//return the result
+		$arr =[];
+		while($row = $res->fetch_assoc()){
+			$arr[] = $row;
+		}
+		return $arr;
+	}
+};
+
+function truncateString($string, $length = 100, $append = "...") {
+    if (strlen($string) <= $length) {
+        return $string;
+    }
+    $truncated = substr($string, 0, $length - strlen($append));
+    return $truncated . $append;
+}
