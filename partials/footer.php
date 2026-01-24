73bfe973085abbf29bd5cb761d04b0133b84c161
Danijel Korent <danijel.korent@gmail.com>
2026-01-24 12:35:30 +0100

    Disabled copyright in footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,5 @@
 <?php //Import constants file
 require_once dirname(__DIR__).'/config.php'; ?>
 <footer class="w-full flex justify-center py-5">
-	<p>&copy; <?= date('Y') ?> <?=SITE_NAME?></p>
+	<!-- <p>&copy; <?= date('Y') ?> <?=SITE_NAME?></p> -->
 </footer>
